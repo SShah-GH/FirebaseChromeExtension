93458bddd934e01bdf02e5f0c770fb83a93018ce
Satva <satvabshah@gmail.com>
2023-04-25 19:47:26 -0700

    Using current firebase location

--- src/popup/src/const.js
+++ src/popup/src/const.js
@@ -1,10 +1,9 @@
-export const FIREBASE_CONFIG = {
-  apiKey: "AIzaThisHasBeenMlRandomizedJMErMve1QtE",
-  authDomain: "<some-id>.firebaseapp.com",
-  databaseURL: "https://<some-id>.firebaseio.com",
-  projectId: "<some-id>",
-  storageBucket: "<some-id>.appspot.com",
-  messagingSenderId: "376725366650",
-  appId: "1:376725366650:web:some-id",
-  measurementId: "G-5C100CL40M"
-}
+export const firebaseConfig = {
+  apiKey: "AIzaSyBVvV9e-BNlGEaJLICnFbF4OKZ3OkyogMc",
+  authDomain: "chromeextensionauth-645df.firebaseapp.com",
+  projectId: "chromeextensionauth-645df",
+  storageBucket: "chromeextensionauth-645df.appspot.com",
+  messagingSenderId: "280190627954",
+  appId: "1:280190627954:web:a4dfc09d83a580b41b52eb",
+  measurementId: "G-1S8C0RW6BD"
+};
