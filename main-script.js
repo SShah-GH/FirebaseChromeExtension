acc0c15f38efbfd0ec827fff8a2d7b466806092f
Satva <satvabshah@gmail.com>
2023-04-25 19:22:13 -0700

    Delete old code

--- main-script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('#sign_out').addEventListener('click', () => {
-    chrome.runtime.sendMessage({ message: 'sign_out' }, function (response) {
-        if (response.message === 'success') {
-            window.location.replace('./popup.html');
-        }
-    });
-});
