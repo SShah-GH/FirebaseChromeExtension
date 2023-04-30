fa2b94e3da76fae9a6177e304db591fe5d0790f7
Satva <satvabshah@gmail.com>
2023-04-30 09:57:57 -0700

    Reverted to old credentials

--- src/popup/src/const.js
+++ src/popup/src/const.js
@@ -1,9 +1,9 @@
-export const firebaseConfig = {
+export const FIREBASE_CONFIG = {
   apiKey: "AIzaSyBVvV9e-BNlGEaJLICnFbF4OKZ3OkyogMc",
   authDomain: "chromeextensionauth-645df.firebaseapp.com",
   projectId: "chromeextensionauth-645df",
   storageBucket: "chromeextensionauth-645df.appspot.com",
   messagingSenderId: "280190627954",
   appId: "1:280190627954:web:a4dfc09d83a580b41b52eb",
-  measurementId: "G-1S8C0RW6BD"
+  measurementId: "G-1S8C0RW6BD",
 };
